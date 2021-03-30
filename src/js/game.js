2e9f1379964e47dcbb15ee14fc5b7e9801c72d5c
Ehan <ethanliang405@gmail.com>
2021-03-29 21:03:17 -0400

    question 1 and following question green next button

--- src/js/game.js
+++ src/js/game.js
@@ -1,118 +1,133 @@
-console.log("Connected");
+import { DOMSelectors } from "./DOM";
+import { questionsList } from "./questions.js";
+import { question1 } from "./questionFunctions.js";
+import { question2 } from "./questionFunctions.js";
+import { question3 } from "./questionFunctions.js";
+import { question4 } from "./questionFunctions.js";
+import { question5 } from "./questionFunctions.js";
+import { question6 } from "./questionFunctions.js";
+import { question7 } from "./questionFunctions.js";
+import { question8 } from "./questionFunctions.js";
+import { question9 } from "./questionFunctions.js";
+import { question10 } from "./questionFunctions.js";
 
-//Max 13 choices
-let questionsList = [
-  {
-    questionNum: 1,
-    question: "What kind of stuff do you like to watch?",
-    //value: 3,
-    choice1: "Romance or drama", //CAN INCLUDE: Comedy, drama, music, mystery, romance, slice of life. DO YOU LIKE ELEMENTS OF MYSTERY
-    choice2: "Fight scenes", //Action, comedy, drama, horror, mystery, sports SOMETHING FUNNY OR SOMETHING DARK
-    choice3: "everyday life", //Comedy, drama, music, romance, slice of life SOMETHING LAID BACK OR SOMETHING WITH DRAMA
-    choice4: "Something that will keep me on the edge of the chair", //Action, comedy, drama, horror, mystery, sports HORROR? OR COMPETITION?
-    choice5: "Someone painting or playing music", //Comedy, music, slice of life
-    choice6: "Youtubers playing video games", //Action, comedy, sports
-    choice7: "None of these", //go read a book
-  },
-  {
-    question: "Do you want elements of mystery or suspense?", //IF choice1
-    //value: 3,
-    choice1: "Yes", //Must have mystery
-    choice2: "No", //Must not have
-    choice3: "Don't care", //Can be both (no action)
-  },
-  {
-    question: "Do you want something funny or something dark?", //IF choice2
-    //value: 3,
-    choice1: "Funny",
-    choice2: "Dark",
-    choice3: "Both funny and dark",
-    choice4: "Don't care",
-  },
-  {
-    question: "Do you want something laid-back or something with drama?", //IF choice3    //value: 3,
-    choice1: "Something chill", //Must not have drama
-    choice2: "Drama", //Must have drama
-    choice3: "Don't care", //Can be both (no action)
-  },
-  {
-    question: "Do you want anything dark or scary?", //IF choice4
-    //value: 3,
-    choice1: "Yes", //Must include horror
-    choice2: "No", //Must not include horror
-    choice3: "Don't care", //No action
-  },
-  /*   {
-    question: "What genre are you looking for?",
-    //value: 3,
-    choice1: "Action",
-    choice2: "Comedy",
-    choice3: "Drama",
-    choice4: "Horror",
-    choice5: "Music",
-    choice6: "Mystery",
-    choice7: "Romance",
-    choice8: "Slice of Life",
-    choice9: "Sports",
-  }, */
-  {
-    question:
-      "Which of these genres do you want your recommendations to include?", //checklist
-    //value: 3,
-    choice1: "Ecchi",
-    choice2: "Magic",
-    choice3: "Psychological",
-    choice4: "School",
-    choice5: "Sci-fi",
-    choice6: "Supernatural",
-    choice7: "Thriller",
-    choice8: "Vampires",
-    choice9: "None of these",
-  },
-  {
-    question: "Any of these that you want to see?", //checklist
-    //value: 3,
-    //choice1: "Cars",
-    choice1: "Mind-twisting plots", //Dementia
-    choice2: "Demons and dark creatures", //Demons
-    choice3: "Video games or board games", //Game
-    choice4: "Harems", //Harem
-    choice5: "History or historical fiction", //Historical
-    //choice7: "Shows for kids", //Kids
-    //choice8: "Martial arts",
-    choice6: "Giant robots and big machines", //Mecha
-    choice7: "A heavy militaristic feel", //Military
-    choice8: "Parodies", //Parody
-    //choice12: "Police",
-    choice9: "Samurai",
-    choice10: "Space",
-    choice11: "Superpowers", //Superpower
-    choice12: "All of the above",
-  },
-  {
-    question: "Air date?",
-    //value: 3,
-    choice1: "",
-    choice2: "",
-    choice3: "",
-    choice4: "",
-  },
-  {
-    question: "How many episodes would you prefer?", //checklist
-    //value: 3,
-    choice1: "13 or less",
-    choice2: "13 to 26",
-    choice3: "26 or less",
-    choice4: "Greater than 26",
-    choice5: "I'm fine with any length",
-  },
-  {
-    question: "How much anime have you watched?", //SORT BY POPULARITY NOT NETSCORE
-    //value: 3,
-    choice1: "A lot", //Less popular
-    choice2: "I can count them on my fingers", //Not top 10
-    choice3: "000000000", //All
-  },
-];
+//QUESTIONS 1-6
+question1();
 
-//s
+document.getElementById("choice1").addEventListener("click", function () {
+  let correct = document.getElementsByClassName("correct");
+  //console.log(correct);
+  if (correct.length > 0) {
+    correct[0].classList.remove("correct");
+  }
+
+  let addGreen = document.getElementById("choice1");
+  addGreen.classList.add("correct");
+
+  let selectedChoice = 1;
+  document.getElementById("nextButton").addEventListener("click", function () {
+    question2();
+  });
+});
+
+document.getElementById("choice2").addEventListener("click", function () {
+  let correct = document.getElementsByClassName("correct");
+  //console.log(correct);
+  if (correct.length > 0) {
+    correct[0].classList.remove("correct");
+  }
+
+  let addGreen = document.getElementById("choice2");
+  addGreen.classList.add("correct");
+
+  let selectedChoice = 1;
+  document.getElementById("nextButton").addEventListener("click", function () {
+    question3();
+  });
+});
+
+document.getElementById("choice3").addEventListener("click", function () {
+  let correct = document.getElementsByClassName("correct");
+  //console.log(correct);
+  if (correct.length > 0) {
+    correct[0].classList.remove("correct");
+  }
+
+  let addGreen = document.getElementById("choice3");
+  addGreen.classList.add("correct");
+
+  let selectedChoice = 1;
+  document.getElementById("nextButton").addEventListener("click", function () {
+    question4();
+  });
+});
+
+document.getElementById("choice4").addEventListener("click", function () {
+  let correct = document.getElementsByClassName("correct");
+  //console.log(correct);
+  if (correct.length > 0) {
+    correct[0].classList.remove("correct");
+  }
+
+  let addGreen = document.getElementById("choice4");
+  addGreen.classList.add("correct");
+
+  let selectedChoice = 1;
+  document.getElementById("nextButton").addEventListener("click", function () {
+    question5();
+  });
+});
+
+document.getElementById("choice5").addEventListener("click", function () {
+  let correct = document.getElementsByClassName("correct");
+  //console.log(correct);
+  if (correct.length > 0) {
+    correct[0].classList.remove("correct");
+  }
+
+  let addGreen = document.getElementById("choice5");
+  addGreen.classList.add("correct");
+
+  let selectedChoice = 1;
+  document.getElementById("nextButton").addEventListener("click", function () {
+    question6();
+  });
+});
+
+document.getElementById("choice6").addEventListener("click", function () {
+  let correct = document.getElementsByClassName("correct");
+  //console.log(correct);
+  if (correct.length > 0) {
+    correct[0].classList.remove("correct");
+  }
+
+  let addGreen = document.getElementById("choice6");
+  addGreen.classList.add("correct");
+
+  let selectedChoice = 1;
+  document.getElementById("nextButton").addEventListener("click", function () {
+    question6();
+  });
+});
+
+document.getElementById("choice7").addEventListener("click", function () {
+  let correct = document.getElementsByClassName("correct");
+  //console.log(correct);
+  if (correct.length > 0) {
+    correct[0].classList.remove("correct");
+  }
+
+  let addGreen = document.getElementById("choice7");
+  addGreen.classList.add("correct");
+
+  let selectedChoice = 1;
+  document.getElementById("nextButton").addEventListener("click", function () {
+    question6();
+  });
+});
+
+//listen for click, if choice x then question2();
+
+//console.log(DOMSelectors.displayContainer);
+
+/* ssssssssssss */
--- src/js/home.js
+++ src/js/home.js
@@ -1,10 +1,10 @@
 console.log("Connected");
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+/* if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
   const mobileMsg = document.querySelector(".msg");
   msg.innerHTML +
     "<p>If you're on mobile and it looks weird then go on a pc</p>";
-}
+} */
 
 //Random background
 const imagesClass = [
@@ -16,6 +16,17 @@ const imagesClass = [
   "bg-image-nighttrain",
   "bg-image-valley",
   "bg-image-personsunsetdew",
+
+  /*   const images = [
+    "/src/img/duskpowerlines.jpg",
+    "/src/img/fieldsunset.jpg",
+    "/src/img/highsky.jpg",
+    "/src/img/nightbay.jpg",
+    "/src/img/nightcity.jpg",
+    "/src/img/nighttrain.jpg",
+    "/src/img/valley.jpg",
+    "/src/img/personsunsetdew1edit1.jpg",
+  ]; */
 ];
 
 const randomNumberBgImage = Math.floor(Math.random() * imagesClass.length);
@@ -27,17 +38,6 @@ const imgClass = document.getElementsByClassName("bg-image")[0];
 console.log(imgClass);
 
 document.addEventListener("DOMContentLoaded", function () {
-  /*   const images = [
-    "/src/img/duskpowerlines.jpg",
-    "/src/img/fieldsunset.jpg",
-    "/src/img/highsky.jpg",
-    "/src/img/nightbay.jpg",
-    "/src/img/nightcity.jpg",
-    "/src/img/nighttrain.jpg",
-    "/src/img/valley.jpg",
-    "/src/img/personsunsetdew1edit1.jpg",
-  ]; */
-
   imgClass.classList.add(bgImage);
 });
 
